037687288c3c7233c66d334fdfc47fa1b3f11997
whitehatteacher <72393170+whitehatteacher@users.noreply.github.com>
2021-11-19 16:15:31 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,12 @@
+class Player {
+  constructor(){}
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+
+}
